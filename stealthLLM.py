e98eafcedc7ee253ce93b24ff2e86a342f42ad87
d-chandrahas <chandrahasreddy732@gmail.com>
2025-03-23 14:28:53 +0530

    refactoring, added interrupts

--- stealthLLM.py
+++ stealthLLM.py
@@ -1,5 +1,6 @@
 from os import environ as env
 from time import sleep
+from threading import Thread
 from pyperclip import paste
 from pynput.keyboard import Listener, HotKey, Key, KeyCode, Controller, _CONTROL_CODES
 import requests as req
@@ -16,50 +17,47 @@ def handle_prompt(prompt):
         ).json()["candidates"][0]["content"]["parts"][0]["text"]
 
 
-def dummy_handle_prompt(prompt):
-    sleep(2)
-    return "Dummy: " + prompt
-
-
 def canonical_wrapper(fn):
-    global listener
-    return lambda key: fn(listener.canonical(key))
+    global LISTENER
+    return lambda key: fn(LISTENER.canonical(key))
 
 
 def copy_hotkey_handler():
-    global clipboard_events
+    global CLIPBOARD_EVENTS
     sleep(0.1)
-    clipboard_events.append(paste())
+    CLIPBOARD_EVENTS.append(paste())
 
 
 def key_press_handler(key):
-    global key_events
-    key_events.append(key)
+    global KEY_EVENTS
+    KEY_EVENTS.append(key)
 
 
 def write(string, duration=0.0, delay=0.0):
-    global keyboard
+    global STATE, KEYBOARD
+
     for i, character in enumerate(string):
+        if INTERRUPT: break
         key = _CONTROL_CODES.get(character, character)
         try:
-            keyboard.press(key)
+            KEYBOARD.press(key)
             if duration > 0.0:
                 sleep(duration)
-            keyboard.release(key)
+            KEYBOARD.release(key)
             if delay > 0.0:
                 sleep(delay)
 
-        except (ValueError, keyboard.InvalidKeyException):
-            raise keyboard.InvalidCharacterException(i, character)
+        except (ValueError, KEYBOARD.InvalidKeyException):
+            raise KEYBOARD.InvalidCharacterException(i, character)
 
 
 def process_key_events_and_clipboard():
-    global key_events, clipboard_events
-    
+    global KEY_EVENTS, CLIPBOARD_EVENTS, STATE, INTERRUPT
+
     printable_keys = {Key.space: " ", Key.enter: "\n", Key.tab: "\t"}
     chars = []
-    clip_iter = iter(clipboard_events)
-    for event in key_events:
+    clip_iter = iter(CLIPBOARD_EVENTS)
+    for event in KEY_EVENTS:
         if type(event) is KeyCode:
             ascii_code = ord(event.char)
             if ascii_code == 3:
@@ -71,56 +69,70 @@ def process_key_events_and_clipboard():
         elif event is Key.backspace:
             if chars: chars.pop()
     prompt = "".join(chars)
-    write(handle_prompt(prompt), duration=0.05, delay=0.2)
-
+    if prompt:
+        llm_output = handle_prompt(prompt)
+        write(llm_output, duration=0.05, delay=0.2)
+    STATE = "inactive"
+    INTERRUPT = False
 
 
 def invoke_hotkey_handler():
-    global invoke_state, clipboard_events, key_events, press_callback_dispatcher, release_callback_dispatcher, \
-        wrapped_copy_hotkey_press_callback, wrapped_copy_hotkey_release_callback, key_press_handler
+    global STATE, CLIPBOARD_EVENTS, KEY_EVENTS, PRESS_CALLBACK_DISPATCHER, RELEASE_CALLBACK_DISPATCHER, \
+        WRAPPED_COPY_HOTKEY_PRESS_CALLBACK, WRAPPED_COPY_HOTKEY_RELEASE_CALLBACK, key_press_handler
 
-    if not invoke_state:
-        invoke_state = True
-        clipboard_events = []; key_events = []
+    if STATE == "inactive":
+        CLIPBOARD_EVENTS = []; KEY_EVENTS = []
 
-        press_callback_dispatcher.add_callback(wrapped_copy_hotkey_press_callback)
-        release_callback_dispatcher.add_callback(wrapped_copy_hotkey_release_callback)
-        press_callback_dispatcher.add_callback(key_press_handler)
+        PRESS_CALLBACK_DISPATCHER.add_callback(WRAPPED_COPY_HOTKEY_PRESS_CALLBACK)
+        RELEASE_CALLBACK_DISPATCHER.add_callback(WRAPPED_COPY_HOTKEY_RELEASE_CALLBACK)
+        PRESS_CALLBACK_DISPATCHER.add_callback(key_press_handler)
+        STATE = "listening"
+    elif STATE == "listening":
+        PRESS_CALLBACK_DISPATCHER.remove_callback(key_press_handler)
+        PRESS_CALLBACK_DISPATCHER.remove_callback(WRAPPED_COPY_HOTKEY_PRESS_CALLBACK)
+        RELEASE_CALLBACK_DISPATCHER.remove_callback(WRAPPED_COPY_HOTKEY_RELEASE_CALLBACK)
+        STATE = "processing"
+        Thread(target=process_key_events_and_clipboard).start()
+
+
+def interrupt_hotkey_handler():
+    global STATE, INTERRUPT, LISTENER
+    if STATE == "inactive":
+        LISTENER.stop()
+    elif STATE == "listening":
+        PRESS_CALLBACK_DISPATCHER.remove_callback(key_press_handler)
+        PRESS_CALLBACK_DISPATCHER.remove_callback(WRAPPED_COPY_HOTKEY_PRESS_CALLBACK)
+        RELEASE_CALLBACK_DISPATCHER.remove_callback(WRAPPED_COPY_HOTKEY_RELEASE_CALLBACK)
+        STATE = "inactive"
     else:
-        invoke_state = False
-        press_callback_dispatcher.remove_callback(key_press_handler)
-        press_callback_dispatcher.remove_callback(wrapped_copy_hotkey_press_callback)
-        release_callback_dispatcher.remove_callback(wrapped_copy_hotkey_release_callback)
-        process_key_events_and_clipboard()
-
-
-def exit_hotkey_handler():
-    global listener
-    listener.stop()
+        INTERRUPT = True
 
 
 def stealthllm():
-    global keyboard
-    keyboard = Controller()
+    global KEYBOARD
+    KEYBOARD = Controller()
 
-    global invoke_state
-    invoke_state = False
+    global STATE, INTERRUPT
+    STATE = "inactive"
+    INTERRUPT = False
 
-    global wrapped_copy_hotkey_press_callback, wrapped_copy_hotkey_release_callback
+    global WRAPPED_COPY_HOTKEY_PRESS_CALLBACK, WRAPPED_COPY_HOTKEY_RELEASE_CALLBACK
     copy_hotkey = HotKey(HotKey.parse("<ctrl>+c"), copy_hotkey_handler)
-    wrapped_copy_hotkey_press_callback = canonical_wrapper(copy_hotkey.press)
-    wrapped_copy_hotkey_release_callback = canonical_wrapper(copy_hotkey.release)
+    WRAPPED_COPY_HOTKEY_PRESS_CALLBACK = canonical_wrapper(copy_hotkey.press)
+    WRAPPED_COPY_HOTKEY_RELEASE_CALLBACK = canonical_wrapper(copy_hotkey.release)
 
-    global press_callback_dispatcher, release_callback_dispatcher
+    global PRESS_CALLBACK_DISPATCHER, RELEASE_CALLBACK_DISPATCHER
     invoke_hotkey = HotKey(HotKey.parse("<ctrl>+<shift>+q"), invoke_hotkey_handler)
-    exit_hotkey = HotKey(HotKey.parse("<ctrl>+<alt>+q"), exit_hotkey_handler)
-    press_callback_dispatcher = CallbackDispatcher((canonical_wrapper(invoke_hotkey.press), canonical_wrapper(exit_hotkey.press)))
-    release_callback_dispatcher = CallbackDispatcher((canonical_wrapper(invoke_hotkey.release), canonical_wrapper(exit_hotkey.release)))
+    interrupt_hotkey = HotKey(HotKey.parse("<ctrl>+<alt>+q"), interrupt_hotkey_handler)
+    PRESS_CALLBACK_DISPATCHER = CallbackDispatcher((canonical_wrapper(invoke_hotkey.press),
+                                                    canonical_wrapper(interrupt_hotkey.press)))
+    RELEASE_CALLBACK_DISPATCHER = CallbackDispatcher((canonical_wrapper(invoke_hotkey.release),
+                                                      canonical_wrapper(interrupt_hotkey.release)))
 
-    global listener
-    listener = Listener(on_press=press_callback_dispatcher, on_release=release_callback_dispatcher)
-    listener.start()
-    listener.join()
+    global LISTENER
+    LISTENER = Listener(on_press=PRESS_CALLBACK_DISPATCHER, on_release=RELEASE_CALLBACK_DISPATCHER)
+    LISTENER.start()
+    LISTENER.join()
 
 
 class CallbackDispatcher:
@@ -131,7 +143,10 @@ class CallbackDispatcher:
         self.callbacks.append(callback)
 
     def remove_callback(self, callback):
-        self.callbacks.remove(callback)
+        try:
+            self.callbacks.remove(callback)
+        except ValueError:
+            pass
 
     def __call__(self, key):
         for callback in self.callbacks:
